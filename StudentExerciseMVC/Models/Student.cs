9d3bd4c54a15610e21ed768f756fc42240077976
unknown <nwessel429@gmail.com>
2020-01-14 13:27:43 -0600

    added annitations to models

--- StudentExerciseMVC/Models/Student.cs
+++ StudentExerciseMVC/Models/Student.cs
@@ -1,6 +1,7 @@
 ﻿using StudentExercises.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,20 @@ namespace StudentExerciseMVC.Models
 {
     public class Student
     {
+        [Display(Name = "Student Id")]
         public int Id { get; set; }
+        [Required]
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Slack Handle")]
         public string SlackHandle { get; set; }
+        [Display(Name = "Cohort Id")]
         public int CohortId { get; set; }
+        [Display(Name = "Cohort")]
         public Cohort Cohort { get; set; }
         public List<Exercise> Exercises { get; set; } = new List<Exercise>();
     }
