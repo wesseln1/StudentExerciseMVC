dd3e05032c800ea71ea5ca6c232bd0fdc2763ab7
unknown <nwessel429@gmail.com>
2020-01-13 18:39:43 -0600

    repushed student views

--- StudentExerciseMVC/Models/ViewModels/StudentViewModels.cs
+++ StudentExerciseMVC/Models/ViewModels/StudentViewModels.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
 
 namespace StudentExerciseMVC.Models.ViewModels
 {
